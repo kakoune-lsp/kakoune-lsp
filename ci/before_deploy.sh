b633ff42be98fd2ff2c6b94f331d8850649107a6
Ruslan Prokopchuk <fer.obbee@gmail.com>
2018-05-10 11:39:49 +1000

    setup travis for tests and releases; ref #4

---
+++ ci/before_deploy.sh
@@ -0,0 +1,41 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross rustc --bin kak-lsp --target $TARGET --release -- -C lto
+
+    cp target/$TARGET/release/kak-lsp $stage/
+    cp kak-lsp.toml $stage/
+    cp rc/lsp.kak $stage/
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            ;;
+        osx)
+            cp com.github.ul.kak-lsp.plist $stage/
+            ;;
+    esac
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
