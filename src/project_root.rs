kakoune-lsp/kakoune-lsp

commit c13cea5fedcf845eb6bbddc4d455f81fce4a7319

Skip non-existent directories when looking up the project root path
Fixes #302

src/project_root.rs
@@ -14,7 +14,7 @@ pub fn find_project_root(language: &str, markers: &[String], path: &str) -> Stri
 
 pub fn roots_by_marker(roots: &[String], path: &str) -> String {
     let mut pwd = PathBuf::from(path);
-    if pwd.is_file() {
+    while !pwd.is_dir() {
         pwd.pop();
     }
     let src = pwd.to_str().unwrap().to_string();

commit d38643cc1f05cbb83c3b1b702e931217d4c2f80c

Fix infinite loop when editing scratch buffer
Introduced in c13cea5fedc.

Fixes #382

src/project_root.rs
@@ -14,6 +14,10 @@ pub fn find_project_root(language: &str, markers: &[String], path: &str) -> Stri
 
 pub fn roots_by_marker(roots: &[String], path: &str) -> String {
     let mut src = PathBuf::from(path);
+    // For scratch buffers we get a bare filename.
+    if !src.is_absolute() {
+        src = env::current_dir().expect("cannot access current directory");
+    }
     while !src.is_dir() {
         src.pop();
     }