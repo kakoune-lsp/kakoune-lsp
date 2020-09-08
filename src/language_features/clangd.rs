8da398a3669ab7a2e39c501eec911639c665a3d5
topisani <topisani@hamsterpoison.com>
2020-09-08 11:34:03 +0200

    clangd-switch-source-header

---
+++ src/language_features/clangd.rs
@@ -0,0 +1,33 @@
+use lsp_types::request::Request;
+use crate::context::*;
+use crate::types::*;
+use crate::util::*;
+use lsp_types::*;
+
+pub struct SwitchSourceHeaderRequest {}
+
+impl Request for SwitchSourceHeaderRequest {
+    type Params = TextDocumentIdentifier;
+    type Result = Option<Url>;
+    const METHOD: &'static str = "textDocument/switchSourceHeader";
+}
+
+pub fn switch_source_header(meta: EditorMeta, ctx: &mut Context) {
+    let req_params = TextDocumentIdentifier {
+      uri: Url::from_file_path(&meta.buffile).unwrap(),
+    };
+    ctx.call::<SwitchSourceHeaderRequest, _>(
+        meta,
+        req_params,
+        move |ctx: &mut Context, meta, response| match response {
+            Some(response) => {
+              let command = format!(
+                "evaluate-commands -try-client %opt{{jumpclient}} %{{edit {}}}",
+                 editor_quote(response.to_file_path().unwrap().to_str().unwrap()),
+              );
+              ctx.exec(meta, command);
+            }
+            None => return,
+        },
+    );
+}
