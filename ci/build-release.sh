e25771763841fde40950d1c5845670a3d26cabc4
Johannes Altmanninger <aclopte@gmail.com>
2020-11-07 10:05:17 +0100

    Move to GitHub Actions
    See #415

---
+++ ci/build-release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -eux
+
+case $(uname) in
+	Linux) target=x86_64-unknown-linux-musl;;
+	Darwin) target=x86_64-apple-darwin;;
+	*) echo "Unknown target $(uname)";;
+esac
+
+version=$(git describe --tags)
+
+curl -LSfs https://japaric.github.io/trust/install.sh |
+    sh -s -- --force --git rust-embedded/cross --target $target
+command -v cross || PATH=~/.cargo/bin:$PATH
+
+cross build --target $target --release
+cross test  --target $target --release
+
+src=$PWD
+stage=$(mktemp -d)
+
+cp target/$target/release/kak-lsp $stage
+cp kak-lsp.toml $stage
+cp README.asciidoc $stage
+cp COPYING $stage
+cp MIT $stage
+cp UNLICENSE $stage
+
+cd $stage
+tar czf $src/kak-lsp-$version-$target.tar.gz *
+cd $src
+
+rm -rf $stage
