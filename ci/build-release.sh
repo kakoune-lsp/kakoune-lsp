e9a34c21e1a06b5cd4876cd10ce6acd6ac5c5ea9
Johannes Altmanninger <aclopte@gmail.com>
2020-11-07 14:29:55 +0100

    v9.0.0

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-target=${1:-x86_64-unknown-linux-musl}
+target=${1}
 
 version=$(git describe --tags)
 
@@ -22,13 +22,13 @@ curl -LSfs https://japaric.github.io/trust/install.sh |
     sh -s -- --force --git rust-embedded/cross --tag $tag --target $target
 command -v cross || source ~/.cargo/env
 
-# cross build --target $target --release TODO
-# cross test  --target $target --release TODO
+cross build --target $target --release
+cross test  --target $target --release
 
 src=$PWD
 stage=$(mktemp -d)
 
-# cp target/$target/release/kak-lsp $stage TODO
+cp target/$target/release/kak-lsp $stage
 cp kak-lsp.toml $stage
 cp README.asciidoc $stage
 cp COPYING $stage
