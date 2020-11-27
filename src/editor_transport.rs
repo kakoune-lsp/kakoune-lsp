cdf77652f04e0cc4da453fb84967cd80961c569f
Vladimir Bauer <vbauerster@gmail.com>
2020-11-27 20:15:38 +0500

    avoid wrapping 'to_editor' command as it already comes editor_quoted, thus 'eval -verbatim' can be used

--- src/editor_transport.rs
+++ src/editor_transport.rs
@@ -65,27 +65,25 @@ pub fn start(config: &Config, initial_request: Option<String>) -> Result<EditorT
                     .spawn()
                 {
                     Ok(mut child) => {
-                        {
-                            let stdin = child.stdin.as_mut();
-                            if stdin.is_none() {
+                        let stdin = match child.stdin.as_mut() {
+                            Some(stdin) => stdin,
+                            None => {
                                 error!("Failed to get editor stdin");
                                 return;
                             }
-                            let stdin = stdin.unwrap();
-                            let client = response.meta.client.as_ref();
-                            let command = if client.is_some() && !client.unwrap().is_empty() {
-                                format!(
-                                    "eval -client {} {}",
-                                    client.unwrap(),
-                                    editor_quote(&response.command)
-                                )
-                            } else {
-                                response.command.to_string()
-                            };
-                            debug!("To editor `{}`: {}", response.meta.session, command);
-                            if stdin.write_all(command.as_bytes()).is_err() {
-                                error!("Failed to write to editor stdin");
+                        };
+
+                        let client = response.meta.client.as_ref();
+                        let command = match client.filter(|&s| !s.is_empty()) {
+                            Some(client) => {
+                                format!("eval -client {} -verbatim -- {}", client, response.command)
                             }
+                            None => response.command.to_string(),
+                        };
+
+                        debug!("To editor `{}`: {}", response.meta.session, command);
+                        if stdin.write_all(command.as_bytes()).is_err() {
+                            error!("Failed to write to editor stdin");
                         }
                         // code should fail earlier if Kakoune was not spawned
                         // otherwise something went completely wrong, better to panic
