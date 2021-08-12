kakoune-lsp/kakoune-lsp

commit 283c5896ce2fe74a6b2eca9efe217513015cb19a

Avoid some single quotes in editor commands
They make logs a bit noisy because of all the doubling.

src/diagnostics.rs
@@ -93,7 +93,7 @@ pub fn publish_diagnostics(params: Params, ctx: &mut Context) {
         "set buffer lsp_diagnostic_error_count {}; \
          set buffer lsp_diagnostic_warning_count {}; \
          set buffer lsp_errors {} {}; \
-         eval \"set buffer lsp_error_lines {} {} '0| '\"; \
+         eval \"set buffer lsp_error_lines {} {} %[0| ]\"; \
          set buffer lsp_diagnostics {} {}",
         error_count,
         warning_count,

src/text_edit.rs
@@ -214,7 +214,7 @@ pub fn apply_text_edits_to_buffer(
                     KakouneTextEditCommand::Replace => "lsp-replace-selection",
                 };
                 let command = format!(
-                    "exec 'z{}<space>'
+                    "exec \"z{}<space>\"
                     {} {}",
                     if selection_index > 0 {
                         format!("{})", selection_index)
@@ -234,11 +234,11 @@ pub fn apply_text_edits_to_buffer(
 
     let command = format!(
         "select {}
-            exec -save-regs '' Z
+            exec -save-regs \"\" Z
             {}",
         select_edits, apply_edits
     );
-    let command = format!("eval -draft -save-regs '^' {}", editor_quote(&command));
+    let command = format!("eval -draft -save-regs ^ {}", editor_quote(&command));
     uri.and_then(|uri| uri.to_file_path().ok())
         .and_then(|path| {
             path.to_str().map(|buffile| {

commit 350cb9f70a3f80b9dd14a8a21e446d0662378e00

Fix lsp_error_lines quoting error
This regressed in 283c589 ("Avoid some single quotes in editor
commands") - we actually need single quotes here because the string
is double quoted - and for a good reason - it uses %opt expansions.

Fixes #508

src/diagnostics.rs
@@ -93,7 +93,7 @@ pub fn publish_diagnostics(params: Params, ctx: &mut Context) {
         "set buffer lsp_diagnostic_error_count {}; \
          set buffer lsp_diagnostic_warning_count {}; \
          set buffer lsp_errors {} {}; \
-         eval \"set buffer lsp_error_lines {} {} %[0| ]\"; \
+         eval \"set buffer lsp_error_lines {} {} '0| '\"; \
          set buffer lsp_diagnostics {} {}",
         error_count,
         warning_count,