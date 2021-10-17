9783d2e3b2a3cb15fd5ce10358392e25907242d4
Johannes Altmanninger <aclopte@gmail.com>
2021-10-17 18:05:50 +0200

    Ignore error responses to textDocument/codeAction
    At least bash-language-server responds with an error when requesting
    textDocument/codeAction. We show that error in the info box with
    lsp-show-error. Since we don't offer a way to not send the code action
    requests, ignore the error. It will still show up in the log.

--- src/controller.rs
+++ src/controller.rs
@@ -162,7 +162,7 @@ pub fn start(
                                 if let Some(request) = ctx.response_waitlist.remove(&failure.id) {
                                     let (meta, method, _) = request;
                                     match failure.error.code {
-                                        ErrorCode::ServerError(CONTENT_MODIFIED) => {
+                                        code if code == ErrorCode::ServerError(CONTENT_MODIFIED) || method == request::CodeActionRequest::METHOD => {
                                             // Nothing to do, but sending command back to the editor is required to handle case when
                                             // editor is blocked waiting for response via fifo.
                                             ctx.exec(meta, "nop".to_string());
