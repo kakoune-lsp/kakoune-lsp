371d5cceb7ac5ea17211ab4d50870c96a178c97a
Johannes Altmanninger <aclopte@gmail.com>
2021-10-27 10:53:53 +0200

    Add test case demonstrating that code actions work with typescript-language-server
    See #547

---
+++ test/typescript-language-server-code-actions.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# REQUIRES: command -v typescript-language-server
+
+. test/lib.sh
+
+cat > .config/kak-lsp/kak-lsp.toml << EOF
+[language.typescript]
+filetypes = ["typescript"]
+roots = ["*.ts"]
+command = "typescript-language-server"
+args = ["--stdio"]
+EOF
+
+cat > main.ts << EOF
+class MyClass {
+	doSomething() {
+		return false;
+	}
+}
+EOF
+
+test_tmux_kak_start main.ts
+
+test_tmux send-keys /doSomething Enter
+test_sleep
+test_sleep
+test_tmux capture-pane -p | grep -o 💡
+# CHECK: 💡
+
+test_tmux send-keys :lsp-code-actions Enter
+test_sleep
+test_tmux capture-pane -p | grep -o "Infer function return type"
+# CHECK: Infer function return type
+
+test_tmux send-keys Enter
+test_sleep
+test_tmux capture-pane -p | grep -F "doSomething(): boolean {"
+# CHECK: doSomething(): boolean {
