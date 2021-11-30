fe187faf2e817311b24e18ba006221680b1948c6
0phoff <0phoff@users.noreply.github.com>
2021-11-30 15:38:36 +0100

    Allow whitespace in gutter symbols
    By surrounding the different line-specs with single quotes, we allow
    the use of whitespace for the "lsp_diagnostic_line_error_sign",
    "lsp_diagnostic_line_hint_sign", "lsp_diagnostic_line_info_sign",
    "lsp_diagnostic_line_warning_sign" options.
    
    This is not perfect -- if you want a single quote as a symbol you
    need to double it -- but it's good enough for now.
    
    This allows for some breathing room between the diagnostic symbols
    and other gutter columns like line-numbers.

---
+++ test/clangd-diagnostic-gutter.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# REQUIRES: command -v clangd
+
+. test/lib.sh
+
+cat >> .config/kak/kakrc << EOF
+set-option global lsp_diagnostic_line_error_sign   ' X'
+set-option global lsp_diagnostic_line_hint_sign    '¿ '
+set-option global lsp_diagnostic_line_info_sign    '¡ '
+set-option global lsp_diagnostic_line_warning_sign 'W '
+EOF
+
+cat > main.c << EOF
+void main(int argc, char** argv) {}
+syntax error
+EOF
+
+test_tmux_kak_start main.c
+test_tmux capture-pane -p | sed 2q
+# CHECK: {{W }}void main(int argc, char** argv) {}
+# CHECK: {{ X}}syntax error
