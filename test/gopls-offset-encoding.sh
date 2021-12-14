16aa2a1e1a74222b55102d2a6db1e7dde9a40e1e
Johannes Altmanninger <aclopte@gmail.com>
2021-12-14 17:20:49 +0100

    Fix wrong offset_encoding parameter for gopls
    We assume UTF-8 offsets in our default config for gopls,
    but gopls actually implements the spec (UTF-16 offsets).
    The UTF-8 configuration was originally added for bingo, which
    presumably doesn't implement UTF-16 offsets. Remove it.

---
+++ test/gopls-offset-encoding.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# REQUIRES: command -v gopls
+
+. test/lib.sh
+
+cat > main.go << EOF
+package main
+
+func main() {
+	ASCII()
+	BMP()
+	BeyondBMP()
+}
+
+func /*__________*/ ASCII() {
+}
+func /*åååååååååå*/ BMP() {
+}
+func /*🐣🐣🐣🐣🐣*/ BeyondBMP() {
+}
+EOF
+
+test_tmux_kak_start main.go
+
+# This is kak-lsp uses Unicode code points, while gopls uses UTF-16.
+
+# This means that everything in the Basic Multilingual Plane works.
+test_tmux send-keys / BMP Enter gd
+test_sleep
+test_tmux send-keys 'i%()' Escape
+test_sleep_until 'test_tmux capture-pane -p | grep -F "%()"'
+#CHECK: func /*åååååååååå*/ %()BMP() {
+
+# Anything beyond BMP will be off.
+# The chicken symbolize the chicken-and-egg nature of the problem.
+test_tmux send-keys Escape u gk / BeyondBMP Enter gd
+test_sleep
+test_tmux send-keys 'i%()' Escape
+test_sleep_until 'test_tmux capture-pane -p | grep -F "%()"'
+#CHECK: func /*🐣🐣🐣🐣🐣*/ Beyon%()dBMP() {
