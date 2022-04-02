40d3f78f6aad5ce63be805ea551d3988ed5ed077
Johannes Altmanninger <aclopte@gmail.com>
2022-04-02 11:10:14 +0200

    Preserve cursor positions again when applying text edits
    As reported in [1], pylsp formatting moves the cursor to the buffer
    end.  This regressed in 88f52f0c (Avoid shell calls when applying
    text edits, 2022-02-13) this text is inserted with a single "c"
    command (instead of "|cat"). Since pylsp always sends one big text
    that replaces the entire buffer, Kakoune will move the cursor to the
    buffer end.
    
    Since there doesn't appear to be another way to insert data without
    moving the cursor, go back to using | (but just spawn one shell for
    all text edits).
    
    [1]: https://discuss.kakoune.com/t/odd-kak-lsp-pylsp-issue/2029

--- src/util.rs
+++ src/util.rs
@@ -50,6 +50,10 @@ impl Drop for TempFifo {
     }
 }
 
+pub fn shell_quote(s: &str) -> String {
+    format!("'{}'", s.replace("'", r"'\''"))
+}
+
 /// Escape Kakoune string wrapped into single quote
 pub fn editor_escape(s: &str) -> String {
     s.replace("'", "''")

kakoune-lsp/kakoune-lsp

commit 88f52f0c6271df372942b8344ea2bbca43c58b25

Avoid shell calls when applying text edits
As reported on IRC, lsp-formatting is fairly slow at formatting many
lines with PHP's intelephense language server.

This happens because the language server sends many fine
grained text edits, which we apply with lsp-replace-selection or
lsp-insert-selection, both of which shell out to mktemp and cat.

I experimented with this line:

	if (1) ; else ;

which intelephense reformats to

	if (1);
	else;

To reformat 100 of these, we need around 600 calls to
lsp-replace-selection, amounting to 1200 shell calls.  Curiously,
the shell calls are completely unnecessary.  We can simply use insert
mode to perform text edits; we only need to escape keys before passing
them to execute-keys. Do that.
On the reporter's system (with a different input), this makes us
go down from 3m to ~1s.  On my system it's 1 second -> 0.5 seconds
(debug build).

While at it, collapse all edits into a single exec(ute-keys) statement.
This doesn't make a measurable difference system. but perhaps it's
more elegant? Also a bit harder to read, but let's keep it for now.

Alternative/future solution: don't send text edits to Kakoune but apply
all text edits on the server side and just replace the entire buffer.

src/text_edit.rs
@@ -343,7 +343,7 @@ pub fn apply_text_edits_to_buffer<T: TextEditish<T>>(
         .dedup()
         .join(" ");
 
-    let mut apply_edits = edits
+    let edit_keys = edits
         .iter()
         .enumerate()
         .map(
@@ -354,24 +354,24 @@ pub fn apply_text_edits_to_buffer<T: TextEditish<T>>(
                 },
             )| {
                 let command = match command {
-                    KakouneTextEditCommand::InsertBefore => "lsp-insert-before-selection",
-                    KakouneTextEditCommand::Replace => "lsp-replace-selection",
+                    KakouneTextEditCommand::InsertBefore => 'i',
+                    KakouneTextEditCommand::Replace => 'c',
                 };
                 let command = formatdoc!(
-                    "exec \"z{}<space>\"
-                     {} {}",
+                    "z{}<space>{}{}<esc>",
                     if i > 0 {
                         format!("{})", i)
                     } else {
                         String::new()
                     },
                     command,
-                    editor_quote(new_text)
+                    editor_escape_double_quotes(&escape_keys(new_text)),
                 );
                 command
             },
         )
-        .join("\n");
+        .join("");
+    let mut apply_edits = format!("exec \"{}\"", edit_keys);
 
     let maybe_buffile = uri
         .and_then(|uri| uri.to_file_path().ok())
@@ -503,10 +503,7 @@ mod tests {
         let expected = indoc!(
             r#"eval -draft -save-regs ^ 'select 1.5,1.12 1.15,1.21
                exec -save-regs "" Z
-               exec "z<space>"
-               lsp-replace-selection ''std''
-               exec "z1)<space>"
-               lsp-replace-selection ''ffi::{CStr, CString}'''"#
+               exec "z<space>cstd<esc>z1)<space>cffi::{CStr, CString}<esc>"'"#
         )
         .to_string();
         assert_eq!(result, Some(expected));
@@ -521,10 +518,7 @@ mod tests {
         let expected = indoc!(
             r#"eval -draft -save-regs ^ 'select 1.2,1.2 1.3,1.3
                exec -save-regs "" Z
-               exec "z<space>"
-               lsp-insert-before-selection ''inserted''
-               exec "z1)<space>"
-               lsp-replace-selection ''replaced'''"#
+               exec "z<space>iinserted<esc>z1)<space>creplaced<esc>"'"#
         )
         .to_string();
         assert_eq!(result, Some(expected));
@@ -556,12 +550,7 @@ mod tests {
         let expected = indoc!(
             r#"eval -draft -save-regs ^ 'select 1.5,1.9 1.11,1.13 2.9,2.14
                exec -save-regs "" Z
-               exec "z<space>"
-               lsp-replace-selection ''if''
-               exec "z1)<space>"
-               lsp-replace-selection ''let Test::Foo = foo''
-               exec "z2)<space>"
-               lsp-replace-selection ''println'''"#
+               exec "z<space>cif<esc>z1)<space>clet Test::Foo = foo<esc>z2)<space>cprintln<esc>"'"#
         )
         .to_string();
         assert_eq!(result, Some(expected));
@@ -629,33 +618,14 @@ mod tests {
         let expected = indoc!(
             r#"eval -draft -save-regs ^ 'select 1.5,1.19 2.1,2.24 4.4,4.7 5.9,5.15 5.17,5.17 5.19,5.51 5.53,7.6 7.8,7.36 7.38,7.39 8.2,13.1000000
                exec -save-regs "" Z
-               exec "z<space>"
-               lsp-replace-selection ''std::{path::Path, process::Stdio}''
-               exec "z1)<space>"
-               lsp-replace-selection ''
+               exec "z<space>cstd::{path::Path, process::Stdio}<esc>z1)<space>c
                fn main() {
-                   let matches = App::new("kak-lsp").get_matches();
+                   let matches = App::new(""kak-lsp"").get_matches();
 
-                   if matches.is_present("kakoune") {}
-               }''
-               exec "z2)<space>"
-               lsp-replace-selection ''kakoune''
-               exec "z3)<space>"
-               lsp-replace-selection ''script:''
-               exec "z4)<space>"
-               lsp-insert-before-selection ''&str ''
-               exec "z5)<space>"
-               lsp-replace-selection ''include_str!("../rc/lsp.kak")''
-               exec "z6)<space>"
-               lsp-replace-selection ''
-                   let''
-               exec "z7)<space>"
-               lsp-replace-selection ''args''
-               exec "z8)<space>"
-               lsp-replace-selection ''= env::args().skip(1);''
-               exec "z9)<space>"
-               lsp-replace-selection ''
-               '''"#
+                   if matches.is_present(""kakoune"") {}
+               }<esc>z2)<space>ckakoune<esc>z3)<space>cscript:<esc>z4)<space>i&str <esc>z5)<space>cinclude_str!(""../rc/lsp.kak"")<esc>z6)<space>c
+                   let<esc>z7)<space>cargs<esc>z8)<space>c= env::args().skip(1);<esc>z9)<space>c
+               <esc>"'"#
         ).to_string();
         assert_eq!(result, Some(expected));
     }

src/util.rs
@@ -71,6 +71,11 @@ pub fn editor_quote_double_quotes(s: &str) -> String {
     format!("\"{}\"", editor_escape_double_quotes(s))
 }
 
+// Escape a sequence of printable keys so they can safely be passed to "execute-keys".
+pub fn escape_keys(s: &str) -> String {
+    return s.replace("<", "<lt>");
+}
+
 /// Escape Kakoune tuple element, as used in option types "completions", "line-specs" and
 /// "range-specs".
 pub fn escape_tuple_element(s: &str) -> String {

rc/lsp.kak
@@ -1516,31 +1516,6 @@ define-command -hidden lsp-show-message-log -params 1 -docstring %{
     echo -debug "kak-lsp: log:" %arg{1}
 }
 
-define-command -hidden lsp-insert-before-selection -params 1 -docstring %{
-    Insert content before current selections while keeping cursor intact.
-    It is used to apply text edits from language server.
-} %{
-    declare-option -hidden str lsp_text_edit_tmp %sh{ mktemp }
-    echo -to-file %opt{lsp_text_edit_tmp} -- %arg{1}
-    execute-keys "!cat $kak_opt_lsp_text_edit_tmp; rm $kak_opt_lsp_text_edit_tmp<ret>"
-}
-
-define-command -hidden lsp-replace-selection -params 1 -docstring %{
-    Replace content of current selections while keeping cursor intact.
-    It is used to apply text edits from language server.
-} %{
-    declare-option -hidden str lsp_text_edit_tmp %sh{ mktemp }
-    echo -to-file %opt{lsp_text_edit_tmp} -- %arg{1}
-    try %{
-        execute-keys -draft <a-k>\n\z<ret>
-        execute-keys "|cat $kak_opt_lsp_text_edit_tmp; rm $kak_opt_lsp_text_edit_tmp<ret>"
-    } catch %{
-        # The input has no newline. Kakoune would add one to the input, but also strip it from
-        # the output. Account for that.
-        execute-keys "|cat $kak_opt_lsp_text_edit_tmp; echo; rm $kak_opt_lsp_text_edit_tmp<ret>"
-    }
-}
-
 define-command -hidden lsp-handle-progress -params 6 -docstring %{
   lsp-handle-progress <token> <title> <cancelable> <message> <percentage> <done>
   Handle progress messages sent from the language server. Override to handle this.

commit 40d3f78f6aad5ce63be805ea551d3988ed5ed077

Preserve cursor positions again when applying text edits
As reported in [1], pylsp formatting moves the cursor to the buffer
end.  This regressed in 88f52f0c (Avoid shell calls when applying
text edits, 2022-02-13) this text is inserted with a single "c"
command (instead of "|cat"). Since pylsp always sends one big text
that replaces the entire buffer, Kakoune will move the cursor to the
buffer end.

Since there doesn't appear to be another way to insert data without
moving the cursor, go back to using | (but just spawn one shell for
all text edits).

[1]: https://discuss.kakoune.com/t/odd-kak-lsp-pylsp-issue/2029

src/language_features/range_formatting.rs
@@ -41,13 +41,7 @@ pub fn editor_range_formatting<T: TextEditish<T>>(
     ctx: &mut Context,
 ) {
     let cmd = ctx.documents.get(&meta.buffile).and_then(|document| {
-        apply_text_edits_to_buffer(
-            &meta.client,
-            None,
-            text_edits,
-            &document.text,
-            ctx.offset_encoding,
-        )
+        apply_text_edits_to_buffer(&meta, None, text_edits, &document.text, ctx.offset_encoding)
     });
     match cmd {
         Some(cmd) => ctx.exec(meta, cmd),

src/text_edit.rs
@@ -71,7 +71,7 @@ pub fn apply_annotated_text_edits<T: TextEditish<T>>(
     {
         let meta = meta.clone();
         match apply_text_edits_to_buffer(
-            &meta.client,
+            &meta,
             Some(uri),
             edits,
             &document.text,
@@ -386,19 +386,45 @@ pub fn lsp_text_edits_to_kakoune<T: TextEditish<T>>(
 }
 
 pub fn apply_text_edits_to_buffer<T: TextEditish<T>>(
-    client: &Option<String>,
+    meta: &EditorMeta,
     uri: Option<&Url>,
     text_edits: Vec<T>,
     text: &Rope,
     offset_encoding: OffsetEncoding,
 ) -> Option<String> {
-    let apply_edits = lsp_text_edits_to_kakoune(text_edits, text, offset_encoding)?;
+    let mut apply_edits = lsp_text_edits_to_kakoune(text_edits, text, offset_encoding)?;
+
+    // Apply edits via the | command to preserve cursor positions.
+    let tmpfile = temp_dir().join(format!("{}.edit", meta.session));
+    let tmpfile = tmpfile.to_str().unwrap();
+    let write_result = formatdoc!(
+        r#"set-register t %val[selections_desc]
+           execute-keys %[%"sy]
+           edit -scratch
+           execute-keys %["sRa.<esc>]
+           select %reg[t]
+           {}
+           write {}
+           delete-buffer"#,
+        apply_edits,
+        editor_escape(tmpfile)
+    );
+    apply_edits = formatdoc!(
+        r#"
+         evaluate-commands -save-regs st -no-hooks {}
+         execute-keys {}"#,
+        editor_quote(&write_result),
+        editor_quote(&format!(
+            r#"%|content=$(cat {0}); printf %s "${{content%.}}"; rm {0}<ret>"#,
+            shell_quote(tmpfile)
+        ))
+    );
 
     let maybe_buffile = uri
         .and_then(|uri| uri.to_file_path().ok())
         .and_then(|path| path.to_str().map(|buffile| buffile.to_string()));
 
-    let client = match client {
+    let client = match &meta.client {
         None => {
             return Some(
                 maybe_buffile

src/util.rs
@@ -50,6 +50,10 @@ impl Drop for TempFifo {
     }
 }
 
+pub fn shell_quote(s: &str) -> String {
+    format!("'{}'", s.replace("'", r"'\''"))
+}
+
 /// Escape Kakoune string wrapped into single quote
 pub fn editor_escape(s: &str) -> String {
     s.replace("'", "''")