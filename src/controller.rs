kakoune-lsp/kakoune-lsp

commit 4ee7763aa04e72e9145ddd86346a5c813a1eebaa

Add shim support for workspace/workspaceFolders
LSP has two deprecated alternatives (rootPath and rootUri).  We don't
currently support multiple folders, so there hasn't been much incentive
to implement workspace/workspaceFolders.  However, some language servers [*]
require it. So let's just always send the root path.

[*] see #442, #443, and https://github.com/bash-lsp/bash-language-server/issues/449

src/controller.rs
@@ -377,6 +377,14 @@ fn dispatch_server_request(request: MethodCall, ctx: &mut Context) {
         request::ApplyWorkspaceEdit::METHOD => {
             workspace::apply_edit_from_server(request.params, ctx)
         }
+        request::WorkspaceFoldersRequest::METHOD => {
+            Ok(serde_json::to_value(vec![WorkspaceFolder {
+                uri: Url::from_file_path(&ctx.root_path).unwrap(),
+                name: ctx.root_path.to_string(),
+            }])
+            .ok()
+            .unwrap())
+        }
         request::WorkDoneProgressCreate::METHOD => {
             progress::work_done_progress_create(request.params, ctx)
         }

src/general.rs
@@ -76,7 +76,7 @@ pub fn initialize(root_path: &str, meta: EditorMeta, ctx: &mut Context) {
                 execute_command: Some(DynamicRegistrationClientCapabilities {
                     dynamic_registration: Some(false),
                 }),
-                workspace_folders: Some(false),
+                workspace_folders: Some(true),
                 configuration: Some(true),
                 semantic_tokens: None,
                 code_lens: Some(CodeLensWorkspaceClientCapabilities {
@@ -319,7 +319,10 @@ pub fn initialize(root_path: &str, meta: EditorMeta, ctx: &mut Context) {
         root_uri: Some(Url::from_file_path(root_path).unwrap()),
         root_path: Some(root_path.to_string()),
         trace: Some(TraceOption::Off),
-        workspace_folders: None,
+        workspace_folders: Some(vec![WorkspaceFolder {
+            uri: Url::from_file_path(root_path).unwrap(),
+            name: root_path.to_string(),
+        }]),
         client_info: Some(ClientInfo {
             name: env!("CARGO_PKG_NAME").to_string(),
             version: Some(env!("CARGO_PKG_VERSION").to_string()),

commit 5232b879eae34c3070561770472bbd39f7e5a057

Ignore server request client/registerCapability
Commit 4ee7763a (Add shim support for workspace/workspaceFolders, 2022-07-09)
broke pyright (example configuration in #625) because it made that server
send client/registerCapability, which we don't support yet.  Apparently this server
also crashes in that case. So let's just fake support for client/registerCapability,
to keep basic scenarios working.

src/controller.rs
@@ -377,6 +377,21 @@ fn dispatch_server_request(request: MethodCall, ctx: &mut Context) {
         request::ApplyWorkspaceEdit::METHOD => {
             workspace::apply_edit_from_server(request.params, ctx)
         }
+        request::RegisterCapability::METHOD => {
+            let params: RegistrationParams = request
+                .params
+                .parse()
+                .expect("Failed to parse RegistrationParams params");
+            for registration in params.registrations {
+                // Since we only support one root path, we are never going to send
+                // "workspace/didChangeWorkspaceFolders" anyway, so let's not issue a warning.
+                if registration.method == notification::DidChangeWorkspaceFolders::METHOD {
+                    continue;
+                }
+                warn!("Unsupported registration: {}", registration.method);
+            }
+            Ok(serde_json::Value::Null)
+        }
         request::WorkspaceFoldersRequest::METHOD => {
             Ok(serde_json::to_value(vec![WorkspaceFolder {
                 uri: Url::from_file_path(&ctx.root_path).unwrap(),