5f126707df4d516e90f7c2062fb0ecc2a1c90677
Johannes Altmanninger <aclopte@gmail.com>
2023-02-21 07:55:33 +0100

    Add step to publish on crates.io

--- ci/commit-new-release.sh
+++ ci/commit-new-release.sh
@@ -24,6 +24,7 @@ sed -i "1s/Unreleased/$new - $(date --iso)/" CHANGELOG.md
 cargo check # update Cargo.lock
 git commit -am "v$new"
 git tag "v$new" -m "v$new"
+cargo publish
 sed -i "0,/version/ s/$new/$new-snapshot/" Cargo.toml
 cargo check # update Cargo.lock
 sed -i 1i'## Unreleased\n' CHANGELOG.md
