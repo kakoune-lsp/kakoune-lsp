510abc1247b785e8fe80cf9bb1aa16d0f2e91b0d
Johannes Altmanninger <aclopte@gmail.com>
2023-11-07 22:17:04 +0100

    test: adapt to new rust-analyzer behavior
    They send more code actions now, work around this by filtering.

--- test/rust-analyzer-code-actions.sh
+++ test/rust-analyzer-code-actions.sh
@@ -39,6 +39,8 @@ test_tmux send-keys :lsp-code-actions Enter
 test_sleep_until 'test_tmux capture-pane -p | grep Replace'
 # CHECK: {{.*}}Replace match with if let{{.*}}
 
+test_tmux send-keys iflet
+test_sleep
 test_tmux send-keys Enter
 test_sleep_until 'test_tmux capture-pane -p | grep -q if.let.Test'
 test_tmux capture-pane -p
@@ -54,6 +56,8 @@ test_tmux send-keys :lsp-code-actions Enter
 test_sleep_until 'test_tmux capture-pane -p | grep Replace'
 # CHECK: {{.*}}Replace if let with match{{.*}}
 
+test_tmux send-keys iflet
+test_sleep
 test_tmux send-keys Enter
 test_sleep_until 'test_tmux capture-pane -p | grep -q match.foo'
 test_tmux capture-pane -p
