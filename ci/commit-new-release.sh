2f725d4a8650bec734387006c3239c52264429ba
Johannes Altmanninger <aclopte@gmail.com>
2024-04-05 08:55:01 +0200

    ci/commit-new-release.sh: inline the update-homebrew script
    It should never be called by itself.

--- ci/commit-new-release.sh
+++ ci/commit-new-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 new=$1 # Pass the version numbers, like "1.2.3"
 
@@ -31,6 +31,7 @@ cargo check # update Cargo.lock
 git commit -am "v$new"
 git tag "v$new" --message="$(ci/latest-changelog.sh)"
 cargo publish
+
 sed -i "0,/version/ s/$new/$new-snapshot/" Cargo.toml
 cargo check # update Cargo.lock
 sed -i 1i'## Unreleased\n' CHANGELOG.md
@@ -38,4 +39,26 @@ git commit -am 'start new cycle'
 
 git push origin v$new
 
-ci/update-homebrew.sh
+# Update homebrew package.
+url=https://github.com/kakoune-lsp/kakoune-lsp/releases/download/v$new/kakoune-lsp-v$new-x86_64-apple-darwin.tar.gz
+archive=kakoune-lsp-v$new-x86_64-apple-darwin.tar.gz
+while true
+do
+	curl -O -L "$url"
+	if file --brief --mime "$archive" | grep -q application/gzip; then
+		break
+	fi
+	sleep 60
+done
+sha=$(sha256sum "$archive")
+sha=${sha%% *}
+(
+	cd ../homebrew-kakoune-lsp/
+	sed 4c"  url \"$url\"" -i Formula/kakoune-lsp.rb
+	sed 5c"  sha256 \"$sha\"" -i Formula/kakoune-lsp.rb
+	sed 6c"  version \"$new\"" -i Formula/kakoune-lsp.rb
+	sed '4,6s/^/  /' -i Formula/kakoune-lsp.rb
+
+	git commit -am "v$new"
+	git push
+)
