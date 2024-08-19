kakoune-lsp/kakoune-lsp

commit 420cf75495d73c2738f7257497aadd59a86758ed

Make startup error reporting not depend on request from stdin
We already know the session name, so we can send commands that way.

src/main.rs
@@ -31,15 +31,17 @@ mod workspace;
 
 use crate::types::*;
 use crate::util::*;
-use clap::{crate_version, App, Arg, ArgAction, ArgMatches};
+use clap::ArgMatches;
+use clap::{crate_version, App, Arg, ArgAction};
+use context::meta_for_session;
 use daemonize::Daemonize;
+use editor_transport::send_command_to_editor;
 use fs4::FileExt;
 use itertools::Itertools;
 use sloggers::file::FileLoggerBuilder;
 use sloggers::terminal::{Destination, TerminalLoggerBuilder};
 use sloggers::types::Severity;
 use sloggers::Build;
-use std::borrow::Cow;
 use std::env;
 use std::ffi::CString;
 use std::fs;
@@ -165,7 +167,16 @@ fn main() {
     let mut config: Config = match toml::from_str(&config) {
         Ok(cfg) => cfg,
         Err(err) => {
-            consume_stdin_and_report_config_error(&matches, &session, &err);
+            let command = format!(
+                "lsp-show-error {}",
+                editor_quote(&format!("failed to parse config file: {}", err)),
+            );
+            if let Err(err) = send_command_to_editor(EditorResponse {
+                meta: meta_for_session(session, None),
+                command: command.into(),
+            }) {
+                println!("Failed to send lsp-show-error command to editor: {}", err);
+            }
             panic!("invalid configuration: {}", err)
         }
     };
@@ -287,7 +298,7 @@ fn spin_up_server(input: &[u8]) {
     child.wait().expect("Failed to daemonize server");
 }
 
-fn setup_logger(config: &Config, matches: &clap::ArgMatches) -> slog_scope::GlobalLoggerGuard {
+fn setup_logger(config: &Config, matches: &ArgMatches) -> slog_scope::GlobalLoggerGuard {
     let mut verbosity = matches.get_count("v") as u8;
 
     if verbosity == 0 {
@@ -318,45 +329,6 @@ fn setup_logger(config: &Config, matches: &clap::ArgMatches) -> slog_scope::Glob
     }));
 
     slog_scope::set_global_logger(logger)
-}
-
-fn consume_stdin_and_report_config_error(
-    matches: &ArgMatches,
-    session: &str,
-    error: &toml::de::Error,
-) {
-    if !matches.is_present("initial-request") && !matches.is_present("request") {
-        return; // Don't know how to reach the editor.
-    }
-
-    let mut input = Vec::new();
-    stdin()
-        .read_to_end(&mut input)
-        .expect("Failed to read stdin");
-
-    let data = String::from_utf8_lossy(&input).to_string();
-    let request: EditorRequest = toml::from_str(&data).expect("Failed to parse request");
-    assert!(request.meta.session == session);
-
-    let editor = match editor_transport::start(session, None) {
-        Ok(ed) => ed,
-        Err(_code) => return,
-    };
-    let command = format!(
-        "lsp-show-error {}",
-        editor_quote(&format!("failed to parse config file: {}", error)),
-    );
-    if editor
-        .to_editor
-        .sender()
-        .send(EditorResponse {
-            meta: request.meta,
-            command: Cow::from(command),
-        })
-        .is_err()
-    {
-        error!("Failed to send command to editor");
-    }
 }
 
 // Cleanup and gracefully exit

src/editor_transport.rs
@@ -52,49 +52,8 @@ pub fn start(session: &str, initial_request: Option<String>) -> Result<EditorTra
         channel_capacity,
         move |receiver: Receiver<EditorResponse>, _| {
             for response in receiver {
-                match Command::new("kak")
-                    .args(&["-p", &response.meta.session])
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::null())
-                    .stderr(Stdio::null())
-                    .spawn()
-                {
-                    Ok(mut child) => {
-                        let stdin = match child.stdin.as_mut() {
-                            Some(stdin) => stdin,
-                            None => {
-                                error!("Failed to get editor stdin");
-                                return;
-                            }
-                        };
-
-                        let client = response.meta.client.as_ref();
-                        let command = match client.filter(|&s| !s.is_empty()) {
-                            Some(client) => {
-                                let command = format!(
-                                    "evaluate-commands -client {} -verbatim -- {}",
-                                    client, response.command
-                                );
-                                debug!("To editor `{}`: {}", response.meta.session, command);
-                                Cow::from(command)
-                            }
-                            None => {
-                                debug!(
-                                    "To editor `{}`: {}",
-                                    response.meta.session, response.command
-                                );
-                                response.command
-                            }
-                        };
-
-                        if stdin.write_all(command.as_bytes()).is_err() {
-                            error!("Failed to write to editor stdin");
-                        }
-                        // code should fail earlier if Kakoune was not spawned
-                        // otherwise something went completely wrong, better to panic
-                        child.wait().unwrap();
-                    }
-                    Err(e) => error!("Failed to run Kakoune: {}", e),
+                if let Err(err) = send_command_to_editor(response) {
+                    error!("Failed to send command to editor: {}", err);
                 }
             }
         },
@@ -104,6 +63,58 @@ pub fn start(session: &str, initial_request: Option<String>) -> Result<EditorTra
         from_editor,
         to_editor,
     })
+}
+
+pub fn send_command_to_editor(response: EditorResponse) -> Result<(), String> {
+    match Command::new("kak")
+        .args(&["-p", &response.meta.session])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+    {
+        Ok(mut child) => {
+            let stdin = match child.stdin.as_mut() {
+                Some(stdin) => stdin,
+                None => {
+                    return Err("failed to get editor stdin".to_string());
+                }
+            };
+
+            let client = response.meta.client.as_ref();
+            let command = match client.filter(|&s| !s.is_empty()) {
+                Some(client) => {
+                    let command = format!(
+                        "evaluate-commands -client {} -verbatim -- {}",
+                        client, response.command
+                    );
+                    debug!("To editor `{}`: {}", response.meta.session, command);
+                    Cow::from(command)
+                }
+                None => {
+                    debug!(
+                        "To editor `{}`: {}",
+                        response.meta.session, response.command
+                    );
+                    response.command
+                }
+            };
+
+            if stdin.write_all(command.as_bytes()).is_err() {
+                error!("Failed to write to editor stdin");
+            }
+            // code should fail earlier if Kakoune was not spawned
+            // otherwise something went completely wrong, better to panic
+            let exit_code = child.wait().unwrap();
+            if !exit_code.success() {
+                return Err("kak -p exited with non-zero status".to_string());
+            }
+        }
+        Err(e) => {
+            error!("Failed to run Kakoune: {}", e);
+        }
+    }
+    Ok(())
 }
 
 pub fn start_unix(path: &path::Path, sender: Sender<String>) {

commit 1d02b8f2b4f6ed57e10324b815c0937d3e3288cc

Report config errors in calling client
Regressed in 420cf75 (Make startup error reporting not depend on
request from stdin, 2023-01-28).

src/main.rs
@@ -162,13 +162,21 @@ fn main() {
         .or_else(|| try_config_dir(dirs::preference_dir())) // Historical config dir on macOS.
         ;
 
-    if let Some(config_path) = config_path {
+    if let Some(config_path) = config_path.as_ref() {
         config = fs::read_to_string(config_path).expect("Failed to read config");
     }
 
     let session = String::from(matches.get_one::<String>("session").unwrap());
 
+    let mut raw_request = Vec::new();
+    if matches.get_flag("request") || matches.get_flag("initial-request") {
+        stdin()
+            .read_to_end(&mut raw_request)
+            .expect("Failed to read stdin");
+    }
+
     #[allow(deprecated)]
+    #[allow(clippy::blocks_in_conditions)]
     let mut config: Config = match toml::from_str(&config)
         .map_err(|err| err.to_string())
         .and_then(|mut cfg: Config| {
@@ -197,17 +205,15 @@ fn main() {
         }) {
         Ok(cfg) => cfg,
         Err(err) => {
-            let command = format!(
-                "lsp-show-error {}",
-                editor_quote(&format!("failed to parse config file: {}", err)),
+            report_config_error(
+                &session,
+                &raw_request,
+                format!(
+                    "failed to parse config file {}: {}",
+                    config_path.unwrap().display(),
+                    err
+                ),
             );
-            if let Err(err) = send_command_to_editor(EditorResponse {
-                meta: meta_for_session(session, None),
-                command: command.into(),
-            }) {
-                println!("Failed to send lsp-show-error command to editor: {}", err);
-            }
-            panic!("invalid configuration: {}", err)
         }
     };
 
@@ -217,19 +223,13 @@ fn main() {
         config.server.timeout = timeout.parse().unwrap();
     }
 
-    let mut input = Vec::new();
-    if matches.get_flag("request") || matches.get_flag("initial-request") {
-        stdin()
-            .read_to_end(&mut input)
-            .expect("Failed to read stdin");
-    }
     if matches.get_flag("request") {
         let mut path = util::temp_dir();
         path.push(&config.server.session);
         let connect = || match UnixStream::connect(&path) {
             Ok(mut stream) => {
                 stream
-                    .write_all(&input)
+                    .write_all(&raw_request)
                     .expect("Failed to send stdin to server");
                 true
             }
@@ -248,7 +248,7 @@ fn main() {
             }
         };
         if lockfile.try_lock_exclusive().is_ok() {
-            spin_up_server(&input);
+            spin_up_server(&raw_request);
             if let Err(err) = lockfile.unlock() {
                 println!("Failed to unlock lock file: {:?}", err);
                 goodbye(&config.server.session, 1);
@@ -268,7 +268,7 @@ fn main() {
         // It's important to read input before daemonizing even if we don't use it.
         // Otherwise it will be empty.
         let initial_request = if matches.get_flag("initial-request") {
-            Some(String::from_utf8_lossy(&input).to_string())
+            Some(String::from_utf8_lossy(&raw_request).to_string())
         } else {
             None
         };
@@ -309,7 +309,24 @@ fn kakoune() {
     println!("{}\n{}", script, lsp_cmd);
 }
 
-fn spin_up_server(input: &[u8]) {
+fn report_config_error(session: &str, raw_request: &[u8], error_message: String) -> ! {
+    let editor_request: Option<EditorRequest> =
+        toml::from_str(&String::from_utf8_lossy(raw_request)).ok();
+    let command = format!("lsp-show-error {}", &editor_quote(&error_message));
+    if let Err(err) = send_command_to_editor(EditorResponse {
+        meta: meta_for_session(
+            session.to_string(),
+            editor_request.and_then(|req| req.meta.client),
+        ),
+        command: command.into(),
+    }) {
+        println!("Failed to send lsp-show-error command to editor: {}", err);
+    }
+    println!("{}", error_message);
+    process::exit(1);
+}
+
+fn spin_up_server(raw_request: &[u8]) {
     let args = env::args()
         .filter(|arg| arg != "--request")
         .collect::<Vec<_>>();
@@ -324,7 +341,7 @@ fn spin_up_server(input: &[u8]) {
         .stdin
         .as_mut()
         .unwrap()
-        .write_all(input)
+        .write_all(raw_request)
         .expect("Failed to write initial request");
     child.wait().expect("Failed to daemonize server");
 }