kakoune-lsp/kakoune-lsp

commit 3a87b3db460d64b29edf9c1f587b074facac014c

Discard requests from scratch buffers
If a scratch buffer is given an LSP-enabled filetype, we might end
up crashing as Url::from_file_path() will fail. I don't think there
is any use for scratch buffers yet so let's filter them out before
we have a chance to crash.

This was found on a file opener that uses prompt -on-change to
preview files.

src/controller.rs
@@ -167,6 +167,19 @@ pub fn start(
                     break 'event_loop;
                 }
                 let msg = msg.unwrap();
+                if !msg.meta.buffile.starts_with("/") {
+                    debug!(
+                        "Unsupported scratch buffer, ignoring request from buffer '{}'",
+                        msg.meta.buffile
+                    );
+                    let command = if msg.meta.hook {
+                        "nop"
+                    } else {
+                        "lsp-show-error 'unsupported scratch buffer'"
+                    };
+                    ctx.exec(msg.meta, command);
+                    continue;
+                }
                 // initialize request must be first request from client to language server
                 // initialized response contains capabilities which we save for future use
                 // capabilities also serve as a marker of completing initialization

commit a83a5b6bb361aacdbcfb1114b6b7455809c91e4d

Fix exit not being sent on lsp-stop
We send a request with `method="exit"` to all controllers with a
meta_for_session() meta, with no filename.  Commit 3a87b3d (Discard
requests from scratch buffers, 2024-01-15) broke the shutdown sequence,
so work around that here.

Technically, a scratch buffer with an empty name is allowed but I'll
worry about that later.

src/controller.rs
@@ -165,7 +165,7 @@ pub fn start(
                     break 'event_loop;
                 }
                 let msg = msg.unwrap();
-                if !msg.meta.buffile.starts_with('/') {
+                if !msg.meta.buffile.is_empty() && !msg.meta.buffile.starts_with('/') {
                     debug!(
                         "Unsupported scratch buffer, ignoring request from buffer '{}'",
                         msg.meta.buffile