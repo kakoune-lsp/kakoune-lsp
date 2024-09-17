kakoune-lsp/kakoune-lsp

commit fe4b48108e2ad9ddae7a6543a1592ae9b6115752

Reuse language server across projects if it supports workspaceFolders
See the conversation in https://github.com/kakoune-lsp/kakoune-lsp/pull/765

As a collateral, fix the behavior when lsp-enable is called in a
scratch buffer.

src/settings.rs
@@ -18,17 +18,18 @@ pub fn request_dynamic_configuration_from_kakoune(
 }
 
 pub fn request_initialization_options_from_kakoune(
+    servers: &[ServerId],
     meta: &EditorMeta,
     ctx: &mut Context,
 ) -> Vec<Option<Value>> {
     request_dynamic_configuration_from_kakoune(meta, ctx);
-    let mut sections = Vec::with_capacity(ctx.language_servers.len());
-    let servers: Vec<_> = ctx.language_servers.keys().cloned().collect();
-    for server_id in &servers {
+    let mut sections = Vec::with_capacity(servers.len());
+    for &server_id in servers {
+        let server_name = &ctx.server(server_id).name;
         let settings = ctx
             .dynamic_config
             .language_server
-            .get(&server_id.name)
+            .get(server_name)
             .and_then(|v| v.settings.as_ref());
         let settings = configured_section(meta, ctx, server_id, settings);
         if settings.is_some() {
@@ -42,9 +43,8 @@ pub fn request_initialization_options_from_kakoune(
             continue;
         }
 
-        let server_config = server_configs(&ctx.config, meta)
-            .get(&server_id.name)
-            .unwrap();
+        let server_name = &ctx.server(server_id).name;
+        let server_config = server_configs(&ctx.config, meta).get(server_name).unwrap();
         let settings = configured_section(meta, ctx, server_id, server_config.settings.as_ref());
         sections.push(settings);
     }
@@ -54,12 +54,13 @@ pub fn request_initialization_options_from_kakoune(
 pub fn configured_section(
     meta: &EditorMeta,
     ctx: &Context,
-    server_id: &ServerId,
+    server_id: ServerId,
     settings: Option<&Value>,
 ) -> Option<Value> {
+    let server_name = &ctx.server(server_id).name;
     settings.and_then(|settings| {
         server_configs(&ctx.config, meta)
-            .get(&server_id.name)
+            .get(server_name)
             .and_then(|cfg| cfg.settings_section.as_ref())
             .and_then(|section| settings.get(section).cloned())
     })
@@ -81,12 +82,11 @@ pub fn record_dynamic_config(meta: &EditorMeta, ctx: &mut Context, config: &str)
         }
     };
     for (server_name, server) in &meta.language_server {
-        ctx.language_servers
-            .get_mut(&ServerId {
-                name: server_name.to_string(),
-            })
-            .unwrap()
-            .settings = server.settings.clone();
+        let server_id = ctx
+            .route_cache
+            .get(&(server_name.clone(), server.root.clone()))
+            .unwrap();
+        ctx.language_servers.get_mut(server_id).unwrap().settings = server.settings.clone();
     }
 }
 

rc/lsp.kak
@@ -1512,11 +1512,14 @@ done
 ) > /dev/null 2>&1 < /dev/null & }
 }
 
-define-command -hidden lsp-exit-editor-session -params 0..1 -docstring %{
-    lsp-exit-editor-session [<session>]: shutdown language servers associated with current (or given) editor session but keep kakoune-lsp session running
+define-command -hidden lsp-exit -params 0..1 -docstring %{
+    lsp-exit [<session>]: shutdown language servers associated with current (or given) editor session
 } %{
     remove-hooks global lsp
-    nop %sh{ (printf %s "
+    nop %sh{
+            # After rename, use the old session name (if given and lsp_cmd has -s, we'll fail anyway).
+            kak_session=${1:-"$kak_session"}
+            (printf %s "
 session  = \"${kak_session}\"
 client   = \"${kak_client}\"
 buffile  = \"${kak_buffile}\"
@@ -1619,25 +1622,6 @@ faces_str = \"\"\"${kak_opt_lsp_semantic_tokens}\"\"\"
 [params]
 edit     = $1
 " | eval "${kak_opt_lsp_cmd} --request" # kak_opt_lsp_debug kak_opt_lsp_timeout kak_opt_lsp_snippet_support kak_opt_lsp_file_watch_support
-) > /dev/null 2>&1 < /dev/null & }
-}
-
-define-command lsp-stop -params 0..1 -docstring "lsp-stop [<session>]: terminate the given kakoune-lsp session" %{
-    remove-hooks global lsp
-    nop %sh{ (printf %s "
-session  = \"${kak_session}\"
-client   = \"${kak_client}\"
-buffile  = \"${kak_buffile}\"
-filetype = \"${kak_opt_filetype}\"
-language_id = \"${kak_opt_lsp_language_id}\"
-version  = ${kak_timestamp:-0}
-method   = \"stop\"
-$([ -z ${kak_hook_param+x} ] || echo hook = true)
-${kak_opt_lsp_servers}
-[semantic_tokens]
-faces = \"\"\"${kak_opt_lsp_semantic_tokens}\"\"\"
-[params]
-" | eval "${kak_opt_lsp_cmd} $([ -n "$1" ] && echo --session=$1) --request" # kak_opt_lsp_debug kak_opt_lsp_timeout kak_opt_lsp_snippet_support kak_opt_lsp_file_watch_support
 ) > /dev/null 2>&1 < /dev/null & }
 }
 
@@ -2548,14 +2532,6 @@ define-command lsp-auto-signature-help-disable -docstring "Disable auto-requesti
     remove-hooks global lsp-auto-signature-help
 }
 
-define-command lsp-stop-on-exit-enable -docstring "End kakoune-lsp session on Kakoune session end" %{
-    alias global lsp-exit lsp-stop
-}
-
-define-command lsp-stop-on-exit-disable -docstring "Don't end kakoune-lsp session on Kakoune session end" %{
-    alias global lsp-exit lsp-exit-editor-session
-}
-
 define-command lsp-inlay-hints-enable -params 1 -docstring "lsp-inlay-hints-enable <scope>: enable inlay hints for <scope>" %{
     add-highlighter "%arg{1}/lsp_inlay_hints" replace-ranges lsp_inlay_hints
     hook -group lsp-inlay-hints %arg{1} BufReload .* lsp-inlay-hints
@@ -2730,7 +2706,6 @@ define-command -hidden lsp-require-enabled -params 1 %{
     %opt{lsp_fail_if_disabled} "%arg{1}: run lsp-enable or lsp-enable-window first"
 }
 
-lsp-stop-on-exit-enable
 hook -always global KakEnd .* lsp-exit-ifn
 
 # SNIPPETS
@@ -3192,3 +3167,7 @@ define-command -hidden lsp-goto-next-match -docstring 'DEPRECATED, use jump-next
 define-command -hidden lsp-goto-previous-match -docstring 'DEPRECATED, use jump-previous. Jump to the previous goto match' %{
     jump-previous '*goto*'
 }
+
+define-command -hidden lsp-stop-on-exit-enable -docstring "DEPRECATED. End kakoune-lsp session on Kakoune session end" %{}
+define-command -hidden lsp-stop-on-exit-disable -docstring "DEPRECATED. Don't end kakoune-lsp session on Kakoune session end" %{}
+define-command -hidden lsp-stop -docstring "DEPRECATED: lsp-stop: terminate the given kakoune-lsp session" lsp-exit

src/context.rs
@@ -1,5 +1,7 @@
+use crate::language_server_transport::LanguageServerTransport;
 use crate::text_sync::CompiledFileSystemWatcher;
-use crate::types::*;
+use crate::thread_worker::Worker;
+use crate::{filetype_to_language_id_map, types::*};
 use crossbeam_channel::Sender;
 use jsonrpc_core::{self, Call, Error, Failure, Id, Output, Success, Value, Version};
 use lsp_types::notification::{Cancel, Notification};
@@ -8,7 +10,7 @@ use lsp_types::*;
 use serde::Deserialize;
 use std::borrow::Cow;
 use std::collections::hash_map::Entry;
-use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
 use std::convert::TryInto;
 use std::path::PathBuf;
 use std::{fs, time};
@@ -43,12 +45,19 @@ pub struct OutstandingRequests {
 }
 
 pub struct ServerSettings {
-    pub root_path: String,
+    pub name: String,
+    pub roots: Vec<RootPath>,
     pub offset_encoding: OffsetEncoding,
     pub preferred_offset_encoding: Option<OffsetEncoding>,
+    pub transport: LanguageServerTransport,
     pub capabilities: Option<ServerCapabilities>,
     pub settings: Option<Value>,
-    pub tx: Sender<ServerMessage>,
+    pub users: Vec<SessionId>,
+}
+
+pub struct FileWatcher {
+    pub pending_file_events: HashSet<FileEvent>,
+    pub worker: Box<Worker<(), Vec<FileEvent>>>,
 }
 
 pub struct Context {
@@ -69,29 +78,27 @@ pub struct Context {
     pub dynamic_config: DynamicConfig,
     pub editor_tx: Sender<EditorResponse>,
     pub language_servers: BTreeMap<ServerId, ServerSettings>,
+    pub route_cache: HashMap<(ServerName, RootPath), ServerId>,
     pub outstanding_requests:
         HashMap<(ServerId, &'static str, String, Option<String>), OutstandingRequests>,
     pub pending_requests: Vec<EditorRequest>,
     pub pending_message_requests: VecDeque<(Id, ServerId, ShowMessageRequestParams)>,
     pub request_counter: u64,
     pub response_waitlist: HashMap<Id, (EditorMeta, &'static str, BatchNumber, bool)>,
-    pub session: SessionId,
+    pub sessions: Vec<SessionId>,
     pub work_done_progress: HashMap<NumberOrString, Option<WorkDoneProgressBegin>>,
     pub work_done_progress_report_timestamp: time::Instant,
     pub pending_file_watchers:
         HashMap<(ServerId, String, Option<PathBuf>), Vec<CompiledFileSystemWatcher>>,
-}
-
-pub struct ContextBuilder {
-    pub language_servers: BTreeMap<ServerId, ServerSettings>,
-    pub initial_request: EditorRequest,
-    pub editor_tx: Sender<EditorResponse>,
-    pub config: Config,
+    pub file_watcher: Option<FileWatcher>,
+    #[deprecated]
+    pub legacy_filetypes: HashMap<String, (LanguageId, Vec<ServerName>)>,
 }
 
 impl Context {
-    pub fn new(params: ContextBuilder) -> Self {
-        let session = params.initial_request.meta.session.clone();
+    pub fn new(session: SessionId, editor_tx: Sender<EditorResponse>, config: Config) -> Self {
+        let legacy_filetypes = filetype_to_language_id_map(&config);
+        #[allow(deprecated)]
         Context {
             batch_count: 0,
             batch_sizes: Default::default(),
@@ -100,22 +107,45 @@ impl Context {
             completion_items: vec![],
             completion_items_timestamp: i32::MAX,
             completion_last_client: None,
-            config: params.config,
+            config,
             diagnostics: Default::default(),
             documents: Default::default(),
             dynamic_config: DynamicConfig::default(),
-            editor_tx: params.editor_tx,
-            language_servers: params.language_servers,
+            editor_tx,
+            language_servers: BTreeMap::new(),
+            route_cache: HashMap::new(),
             outstanding_requests: HashMap::default(),
-            pending_requests: vec![params.initial_request],
+            pending_requests: vec![],
             pending_message_requests: VecDeque::new(),
             request_counter: 0,
             response_waitlist: HashMap::default(),
-            session,
+            sessions: vec![session],
             work_done_progress: HashMap::default(),
             work_done_progress_report_timestamp: time::Instant::now(),
             pending_file_watchers: HashMap::default(),
+            file_watcher: None,
+            legacy_filetypes,
         }
+    }
+
+    pub fn last_session(&self) -> &SessionId {
+        self.sessions.last().unwrap()
+    }
+
+    pub fn main_root<'a>(&'a self, meta: &'a EditorMeta) -> &'a RootPath {
+        &self.servers(meta).next().unwrap().1.roots[0]
+    }
+
+    pub fn servers<'a>(
+        &'a self,
+        meta: &'a EditorMeta,
+    ) -> impl Iterator<Item = (ServerId, &'a ServerSettings)> {
+        meta.servers
+            .iter()
+            .map(move |&server_id| (server_id, self.server(server_id)))
+    }
+    pub fn server(&self, server_id: ServerId) -> &ServerSettings {
+        &self.language_servers[&server_id]
     }
 
     pub fn call<
@@ -132,11 +162,11 @@ impl Context {
     {
         let ops = match params {
             RequestParams::All(params) => {
-                let mut ops = Vec::with_capacity(params.len() * self.language_servers.len());
-                for server_id in self.language_servers.keys() {
+                let mut ops = Vec::with_capacity(params.len() * meta.servers.len());
+                for &server_id in &meta.servers {
                     let params: Vec<_> = params.to_vec();
                     for params in params {
-                        ops.push((server_id.clone(), params));
+                        ops.push((server_id, params));
                     }
                 }
                 ops
@@ -145,7 +175,7 @@ impl Context {
                 .into_iter()
                 .flat_map(|(key, ops)| {
                     let ops: Vec<(ServerId, <R as Request>::Params)> =
-                        ops.into_iter().map(|op| (key.clone(), op)).collect();
+                        ops.into_iter().map(|op| (key, op)).collect();
                     ops
                 })
                 .collect(),
@@ -178,7 +208,7 @@ impl Context {
         self.batch_sizes.insert(
             batch_id,
             ops.iter().fold(HashMap::new(), |mut m, (server_id, _)| {
-                let count = m.entry(server_id.clone()).or_default();
+                let count = m.entry(*server_id).or_default();
                 *count += 1;
                 m
             }),
@@ -213,7 +243,7 @@ impl Context {
                 .insert(id.clone(), (meta.clone(), R::METHOD, batch_id, false));
 
             add_outstanding_request(
-                &server_id,
+                server_id,
                 self,
                 R::METHOD,
                 meta.buffile.clone(),
@@ -227,9 +257,11 @@ impl Context {
                 method: R::METHOD.into(),
                 params: params.unwrap(),
             };
-            let server = &self.language_servers[&server_id];
+            let server = self.server(server_id);
             if server
-                .tx
+                .transport
+                .to_lang_server
+                .sender()
                 .send(ServerMessage::Request(Call::MethodCall(call)))
                 .is_err()
             {
@@ -238,19 +270,32 @@ impl Context {
         }
     }
 
-    pub fn cancel(&mut self, server_id: &ServerId, id: Id) {
+    pub fn cancel(&mut self, server_id: ServerId, id: Id) {
+        if let Some((_meta, method, _batch_id, _canceled)) = self.response_waitlist.get(&id) {
+            debug!(
+                "Canceling request to server {}: {:?} ({})",
+                &self.server(server_id).name,
+                id,
+                method
+            );
+        }
         match self.response_waitlist.get_mut(&id) {
-            Some((_meta, method, _batch_id, canceled)) => {
-                debug!("Canceling request to {server_id} server {id:?} ({method})");
+            Some((_meta, _method, _batch_id, canceled)) => {
                 *canceled = true;
             }
             None => {
-                error!("Failed to cancel request {id:?} to {server_id} server");
+                error!(
+                    "Failed to cancel request {id:?} to server {}",
+                    &self.server(server_id).name,
+                );
             }
         }
         let id = match id {
             Id::Num(id) => id,
-            _ => panic!("expected numeric ID for {} server", server_id),
+            _ => panic!(
+                "expected numeric ID for {} server",
+                &self.server(server_id).name
+            ),
         };
         self.notify::<Cancel>(
             server_id,
@@ -260,7 +305,7 @@ impl Context {
         );
     }
 
-    pub fn reply(&mut self, server_id: &ServerId, id: Id, result: Result<Value, Error>) {
+    pub fn reply(&mut self, server_id: ServerId, id: Id, result: Result<Value, Error>) {
         let output = match result {
             Ok(result) => Output::Success(Success {
                 jsonrpc: Some(Version::V2),
@@ -273,13 +318,19 @@ impl Context {
                 error,
             }),
         };
-        let server = &self.language_servers[server_id];
-        if server.tx.send(ServerMessage::Response(output)).is_err() {
-            error!("Failed to reply to language server {server_id}");
+        let server = &self.server(server_id);
+        if server
+            .transport
+            .to_lang_server
+            .sender()
+            .send(ServerMessage::Response(output))
+            .is_err()
+        {
+            error!("Failed to reply to language server {}", &server.name);
         };
     }
 
-    pub fn notify<N: Notification>(&mut self, server_id: &ServerId, params: N::Params)
+    pub fn notify<N: Notification>(&mut self, server_id: ServerId, params: N::Params)
     where
         N::Params: IntoParams,
     {
@@ -293,13 +344,18 @@ impl Context {
             method: N::METHOD.into(),
             params: params.unwrap(),
         };
-        let server = &self.language_servers[server_id];
+        let server = &self.server(server_id);
         if server
-            .tx
+            .transport
+            .to_lang_server
+            .sender()
             .send(ServerMessage::Request(Call::Notification(notification)))
             .is_err()
         {
-            error!("Failed to send notification to language server {server_id}");
+            error!(
+                "Failed to send notification to language server {}",
+                &server.name,
+            );
         }
     }
 
@@ -341,7 +397,7 @@ impl Context {
 
     pub fn meta_for_buffer(&self, client: Option<String>, buffile: &str) -> Option<EditorMeta> {
         let document = self.documents.get(buffile)?;
-        let mut meta = meta_for_session(self.session.clone(), client);
+        let mut meta = meta_for_session(self.last_session().clone(), client);
         meta.buffile = buffile.to_string();
         meta.version = document.version;
         Some(meta)
@@ -353,7 +409,7 @@ impl Context {
         buffile: &str,
         version: i32,
     ) -> EditorMeta {
-        let mut meta = meta_for_session(self.session.clone(), client);
+        let mut meta = meta_for_session(self.last_session().clone(), client);
         meta.buffile = buffile.to_string();
         meta.version = version;
         meta
@@ -376,55 +432,55 @@ pub fn meta_for_session(session: SessionId, client: Option<String>) -> EditorMet
         semantic_tokens: SemanticTokenConfig::default(),
         server: None,
         word_regex: None,
+        servers: vec![],
     }
 }
 
 fn add_outstanding_request(
-    server_id: &ServerId,
+    server_id: ServerId,
     ctx: &mut Context,
     method: &'static str,
     buffile: String,
     client: Option<String>,
     id: Id,
 ) {
-    let to_cancel =
-        match ctx
-            .outstanding_requests
-            .entry((server_id.clone(), method, buffile, client))
-        {
-            Entry::Occupied(mut e) => {
-                let OutstandingRequests { oldest, youngest } = e.get_mut();
-                if oldest.is_none() {
-                    *oldest = Some(id);
-                    None
-                } else {
-                    let mut tmp = Some(id);
-                    std::mem::swap(youngest, &mut tmp);
-                    tmp
-                }
-            }
-            Entry::Vacant(e) => {
-                e.insert(OutstandingRequests {
-                    oldest: Some(id),
-                    youngest: None,
-                });
+    let to_cancel = match ctx
+        .outstanding_requests
+        .entry((server_id, method, buffile, client))
+    {
+        Entry::Occupied(mut e) => {
+            let OutstandingRequests { oldest, youngest } = e.get_mut();
+            if oldest.is_none() {
+                *oldest = Some(id);
                 None
+            } else {
+                let mut tmp = Some(id);
+                std::mem::swap(youngest, &mut tmp);
+                tmp
             }
-        };
+        }
+        Entry::Vacant(e) => {
+            e.insert(OutstandingRequests {
+                oldest: Some(id),
+                youngest: None,
+            });
+            None
+        }
+    };
     if let Some(id) = to_cancel {
         ctx.cancel(server_id, id);
     }
 }
 
 pub fn remove_outstanding_request(
-    server_id: &ServerId,
+    server_id: ServerId,
     ctx: &mut Context,
     method: &'static str,
     buffile: String,
     client: Option<String>,
     id: &Id,
 ) {
-    let key = (server_id.clone(), method, buffile, client);
+    let key = (server_id, method, buffile, client);
     if let Some(outstanding) = ctx.outstanding_requests.get_mut(&key) {
         if outstanding.youngest.as_ref() == Some(id) {
             outstanding.youngest = None;

src/diagnostics.rs
@@ -10,7 +10,7 @@ use lsp_types::*;
 use std::collections::HashMap;
 use std::fmt::Write as _;
 
-pub fn publish_diagnostics(server_id: &ServerId, params: Params, ctx: &mut Context) {
+pub fn publish_diagnostics(server_id: ServerId, params: Params, ctx: &mut Context) {
     let params: PublishDiagnosticsParams = params.parse().expect("Failed to parse params");
     let path = params.uri.to_file_path().unwrap();
     let buffile = path.to_str().unwrap();
@@ -19,12 +19,12 @@ pub fn publish_diagnostics(server_id: &ServerId, params: Params, ctx: &mut Conte
         .remove(buffile)
         .unwrap_or_default()
         .into_iter()
-        .filter(|(id, _)| id != server_id)
+        .filter(|(id, _)| id != &server_id)
         .collect();
     let params: Vec<_> = params
         .diagnostics
         .into_iter()
-        .map(|d| (server_id.clone(), d))
+        .map(|d| (server_id, d))
         .collect();
     diagnostics.extend(params);
     ctx.diagnostics.insert(buffile.to_string(), diagnostics);
@@ -40,7 +40,7 @@ pub fn publish_diagnostics(server_id: &ServerId, params: Params, ctx: &mut Conte
         .sorted_unstable_by_key(|(_, x)| x.severity)
         .rev()
         .map(|(server_id, x)| {
-            let server = &ctx.language_servers[server_id];
+            let server = ctx.server(*server_id);
             format!(
                 "{}|{}",
                 lsp_range_to_kakoune(&x.range, &document.text, server.offset_encoding),
@@ -74,7 +74,7 @@ pub fn publish_diagnostics(server_id: &ServerId, params: Params, ctx: &mut Conte
         let (_, line_diagnostics) = lines_with_diagnostics
             .entry(diagnostic.range.end.line)
             .or_insert((
-                server_id.clone(),
+                server_id,
                 LineDiagnostics {
                     range_end: diagnostic.range.end,
                     symbols: String::new(),
@@ -107,7 +107,7 @@ pub fn publish_diagnostics(server_id: &ServerId, params: Params, ctx: &mut Conte
     let inlay_diagnostics = lines_with_diagnostics
         .iter()
         .map(|(_, (server_id, line_diagnostics))| {
-            let server = &ctx.language_servers[server_id];
+            let server = ctx.server(**server_id);
             let pos = lsp_position_to_kakoune(
                 &line_diagnostics.range_end,
                 &document.text,
@@ -214,7 +214,6 @@ pub fn gather_line_flags(ctx: &Context, buffile: &str) -> (String, u32, u32, u32
 }
 
 pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
-    let (_, main_settings) = ctx.language_servers.first_key_value().unwrap();
     let content = ctx
         .diagnostics
         .iter()
@@ -222,7 +221,8 @@ pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
             diagnostics
                 .iter()
                 .map(|(server_id, x)| {
-                    let server = &ctx.language_servers[server_id];
+                    let server_id = *server_id;
+                    let server = ctx.server(server_id);
                     let p = match get_kakoune_position(server, filename, &x.range.start, ctx) {
                         Some(position) => position,
                         None => {
@@ -232,11 +232,11 @@ pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
                     };
                     format!(
                         "{}:{}:{}: {}{}: {}{}",
-                        short_file_path(filename, &server.root_path),
+                        short_file_path(filename, ctx.main_root(&meta)),
                         p.line,
                         p.column,
-                        &if ctx.language_servers.len() > 1 {
-                            format!("[{server_id}] ")
+                        &if diagnostics.len() > 1 {
+                            format!("[{}] ", &server.name)
                         } else {
                             "".to_string()
                         },
@@ -251,7 +251,7 @@ pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
                             }
                         },
                         x.message,
-                        format_related_information(x, server_id, server, main_settings, ctx)
+                        format_related_information(x, server, diagnostics.len() > 1, &meta, ctx)
                             .unwrap_or_default()
                     )
                 })
@@ -260,7 +260,7 @@ pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
         .join("\n");
     let command = format!(
         "lsp-show-goto-buffer *diagnostics* lsp-diagnostics {} {}",
-        editor_quote(&main_settings.root_path),
+        editor_quote(ctx.main_root(&meta)),
         editor_quote(&content),
     );
     ctx.exec(meta, command);
@@ -268,9 +268,9 @@ pub fn editor_diagnostics(meta: EditorMeta, ctx: &mut Context) {
 
 pub fn format_related_information(
     d: &Diagnostic,
-    server_id: &ServerId,
     server: &ServerSettings,
-    main_settings: &ServerSettings,
+    label_with_server: bool,
+    meta: &EditorMeta,
     ctx: &Context,
 ) -> Option<String> {
     d.related_information
@@ -291,11 +291,11 @@ pub fn format_related_information(
                         );
                         format!(
                             "{}:{}:{}: {}{}",
-                            short_file_path(filename, &main_settings.root_path),
+                            short_file_path(filename, ctx.main_root(meta)),
                             p.line,
                             p.column,
-                            &if ctx.language_servers.len() > 1 {
-                                format!("[{}] ", &server_id.name)
+                            &if label_with_server {
+                                format!("[{}] ", &server.name)
                             } else {
                                 "".to_string()
                             },

src/language_features/call_hierarchy.rs
@@ -12,14 +12,13 @@ pub fn call_hierarchy_prepare(meta: EditorMeta, params: EditorParams, ctx: &mut 
     let params = CallHierarchyParams::deserialize(params)
         .expect("Params should follow CallHierarchyParams structure");
     let req_params = ctx
-        .language_servers
-        .iter()
+        .servers(&meta)
         .map(|(server_id, server_settings)| {
             let position =
                 get_lsp_position(server_settings, &meta.buffile, &params.position, ctx).unwrap();
             let uri = Url::from_file_path(&meta.buffile).unwrap();
             (
-                server_id.clone(),
+                server_id,
                 vec![CallHierarchyPrepareParams {
                     text_document_position_params: TextDocumentPositionParams {
                         text_document: TextDocumentIdentifier::new(uri),
@@ -102,7 +101,7 @@ fn request_call_hierarchy(
 }
 
 fn format_location(
-    server_id: &ServerId,
+    server_id: ServerId,
     meta: &EditorMeta,
     ctx: &mut Context,
     uri: &Url,
@@ -110,9 +109,9 @@ fn format_location(
     prefix: &str,
     suffix: &str,
 ) -> String {
-    let server = &ctx.language_servers[server_id];
+    let server = ctx.server(server_id);
     let filename = uri.to_file_path().unwrap();
-    let filename = short_file_path(filename.to_str().unwrap(), &server.root_path);
+    let filename = short_file_path(filename.to_str().unwrap(), ctx.main_root(meta));
     let position = get_kakoune_position_with_fallback(server, &meta.buffile, position, ctx);
     format!(
         "{}{}:{}:{}: {}\n",
@@ -158,6 +157,7 @@ fn format_call_hierarchy_calls<'a>(
     result: &'a (ServerId, Option<Vec<impl CallHierarchyCall<'a>>>),
 ) {
     let (server_id, result) = result;
+    let server_id = *server_id;
     let result = match result {
         Some(result) => result,
         None => return,
@@ -226,11 +226,10 @@ fn format_call_hierarchy_calls<'a>(
     } else {
         "lsp-show-outgoing-calls"
     };
-    let server = &ctx.language_servers[server_id];
     let command = format!(
         "{} {} {}",
         command,
-        editor_quote(&server.root_path),
+        editor_quote(ctx.main_root(&meta)),
         editor_quote(&contents),
     );
     ctx.exec(meta, command);

commit 589bd1f55f1aabae052c7e105dd9fa4be39e097b

Don't use server settings from new config if running off legacy kak-lsp.toml
Regressed in fe4b4810 (Reuse language server across projects if it
supports workspaceFolders, 2024-09-04).

Closes #776

src/settings.rs
@@ -81,12 +81,14 @@ pub fn record_dynamic_config(meta: &EditorMeta, ctx: &mut Context, config: &str)
             panic!("{}", msg)
         }
     };
-    for (server_name, server) in &meta.language_server {
-        let server_id = ctx
-            .route_cache
-            .get(&(server_name.clone(), server.root.clone()))
-            .unwrap();
-        ctx.language_servers.get_mut(server_id).unwrap().settings = server.settings.clone();
+    if !is_using_legacy_toml(&ctx.config) {
+        for (server_name, server) in &meta.language_server {
+            let server_id = ctx
+                .route_cache
+                .get(&(server_name.clone(), server.root.clone()))
+                .unwrap();
+            ctx.language_servers.get_mut(server_id).unwrap().settings = server.settings.clone();
+        }
     }
 }
 