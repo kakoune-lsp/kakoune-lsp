kakoune-lsp/kakoune-lsp

commit ddcabff9559658507b8429b71b622cd0519a5a42

Try to implement refresh requests
This kind of fixes codeLens, semanticTokens, inlayHints
not being shown with rust-analyzer until the first change to a file.
(Though for semanticTokens we had a workaround).

This fix currently only works for the first client that triggers a
NormalIdle hook.

Future work is to support workspace/inlineValue/refresh and
workspace/diagnostic/refresh (though it's unclear if that's useful
to us).

See https://github.com/rust-lang/rust-analyzer/issues/14048

rc/lsp.kak
@@ -922,6 +922,13 @@ define-command lsp-code-lens -docstring "apply a code lens from the current sele
     lsp-send kakoune/textDocument/codeLens %val{selection_desc}
 }
 
+define-command -hidden lsp-code-lens-request %{
+    declare-option -hidden int lsp_code_lens_timestamp -1
+    lsp-if-changed-since lsp_code_lens_timestamp %opt{lsp_code_lens_timestamp} %{
+        lsp-send textDocument/codeLens
+    }
+}
+
 define-command lsp-execute-command -params 2 -docstring "lsp-execute-command <command> <args>: execute a server-specific command" %{
     lsp-execute-command-request is-async %arg{@}
 }
@@ -1786,6 +1793,7 @@ define-command -hidden lsp-enable-impl -params 1 %{
     hook -group lsp %arg{1} InsertCompletionHide .+ lsp-completion-accepted
     hook -group lsp %arg{1} NormalIdle .* %{
         lsp-did-change
+        lsp-code-lens-request
         evaluate-commands %sh{
             if $kak_opt_lsp_auto_highlight_references; then echo lsp-highlight-references; fi
             if $kak_opt_lsp_auto_show_code_actions; then echo "lsp-code-actions-background-request"; fi

src/capabilities.rs
@@ -98,14 +98,18 @@ pub fn initialize(meta: EditorMeta, ctx: &mut Context, servers: Vec<ServerId>) {
                             }),
                             workspace_folders: Some(true),
                             configuration: Some(true),
-                            semantic_tokens: None,
+                            semantic_tokens: enum_primitive::Option::Some(
+                                SemanticTokensWorkspaceClientCapabilities {
+                                    refresh_support: Some(true),
+                                },
+                            ),
                             code_lens: Some(CodeLensWorkspaceClientCapabilities {
-                                refresh_support: None,
+                                refresh_support: Some(true),
                             }),
                             file_operations: None,
                             inline_value: None,
                             inlay_hint: Some(InlayHintWorkspaceClientCapabilities {
-                                refresh_support: Some(false),
+                                refresh_support: Some(true),
                             }),
                             diagnostic: None,
                         }),

src/controller.rs
@@ -31,7 +31,7 @@ use crate::{context::*, set_logger};
 use ccls::{
     KakouneCallParams, KakouneInheritanceParams, KakouneMemberParams, KakouneNavigateParams,
 };
-use code_lens::CodeLensOptions;
+use code_lens::{text_document_code_lens, CodeLensOptions};
 use crossbeam_channel::{after, never, tick, Receiver, Select, Sender};
 use indoc::formatdoc;
 use inlay_hints::InlayHintsOptions;
@@ -395,6 +395,7 @@ fn dispatch_fifo_request(
             sync_trailer(&mut meta, state, is_sync);
             params
         }
+        "textDocument/codeLens" => Box::new(()),
         "textDocument/completion" => Box::new(TextDocumentCompletionParams {
             position: state.next(),
             completion: EditorCompletion {
@@ -1463,6 +1464,9 @@ fn dispatch_editor_request(request: EditorRequest, ctx: &mut Context) -> Control
         request::CallHierarchyPrepare::METHOD => {
             call_hierarchy::call_hierarchy_prepare(meta, params.unbox(), ctx);
         }
+        request::CodeLensRequest::METHOD => {
+            text_document_code_lens(meta, ctx);
+        }
         request::Completion::METHOD => {
             completion::text_document_completion(meta, params.unbox(), ctx);
         }
@@ -1721,6 +1725,27 @@ fn dispatch_server_request(
         }
         request::ShowMessageRequest::METHOD => {
             return show_message::show_message_request(meta, server_id, request, ctx);
+        }
+        request::CodeLensRefresh::METHOD => {
+            ctx.exec(
+                meta,
+                "evaluate-commands -buffer * unset-option buffer lsp_code_lens_timestamp",
+            );
+            Ok(serde_json::Value::Null)
+        }
+        request::InlayHintRefreshRequest::METHOD => {
+            ctx.exec(
+                meta,
+                "evaluate-commands -buffer * unset-option buffer lsp_inlay_hints_timestamp",
+            );
+            Ok(serde_json::Value::Null)
+        }
+        request::SemanticTokensRefresh::METHOD => {
+            ctx.exec(
+                meta,
+                "evaluate-commands -buffer * unset-option buffer lsp_semantic_tokens_timestamp",
+            );
+            Ok(serde_json::Value::Null)
         }
         _ => {
             warn!(meta.session, "Unsupported method: {}", method);

src/language_features/semantic_tokens.rs
@@ -80,15 +80,7 @@ pub fn tokens_response(
         None => return,
     };
     let tokens = match tokens {
-        SemanticTokensResult::Tokens(tokens) => {
-            if tokens.data.is_empty() {
-                // Early on, rust-analyzer tends to send empty results. Work around this
-                // by not setting the cached timestamp in this case.  Upstream issue
-                // https://github.com/rust-lang/rust-analyzer/issues/14048
-                return;
-            }
-            tokens.data
-        }
+        SemanticTokensResult::Tokens(tokens) => tokens.data,
         SemanticTokensResult::Partial(partial) => partial.data,
     };
     let mut line = 0;

src/text_sync.rs
@@ -5,7 +5,6 @@ use std::{
 };
 
 use crate::context::*;
-use crate::language_features::code_lens::text_document_code_lens;
 use crate::thread_worker::Worker;
 use crate::types::*;
 use crossbeam_channel::{Receiver, Sender};
@@ -43,7 +42,6 @@ pub fn text_document_did_open(
     for &server_id in &meta.servers {
         ctx.notify::<DidOpenTextDocument>(server_id, params.clone());
     }
-    text_document_code_lens(meta, ctx);
 }
 
 pub fn text_document_did_change(
@@ -84,7 +82,6 @@ pub fn text_document_did_change(
     for &server_id in &meta.servers {
         ctx.notify::<DidChangeTextDocument>(server_id, req_params.clone());
     }
-    text_document_code_lens(meta, ctx);
 }
 
 pub fn text_document_did_close(meta: EditorMeta, ctx: &mut Context) {

commit 7c76d6c71b7866d9c9d4e234fafeff415e6b127f

Request code lens on didOpen again
Regressed in ddcabff9 (Try to implement refresh requests, 2024-09-28).
This does not seem to be necessary for rust-analyzer but it seems
more correct.

rc/lsp.kak
@@ -1067,6 +1067,7 @@ define-command -hidden lsp-did-open %{
         lsp-send textDocument/didOpen %val{selection}
     }
     set-option buffer lsp_timestamp %val{timestamp}
+    lsp-code-lens-request
 }
 
 define-command -hidden lsp-did-close %{