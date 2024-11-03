d220f5d1607ef2d7cc095da0dd55f927464ac47c
Johannes Altmanninger <aclopte@gmail.com>
2024-11-03 09:41:16 +0100

    ci/build-release.sh: build ARM packages for macOS
    According to [1], GitHub action runners now provides macOS M1 systems,
    where we can build ARM packages (and keep building x86_64 packages
    with cross).
    
    [1]: https://docs.github.com/en/actions/using-github-hosted-runners/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
    
    Closes #638

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -3,32 +3,38 @@
 set -eux
 
 case $(uname) in
-	Linux) target=x86_64-unknown-linux-musl;;
-	Darwin) target=x86_64-apple-darwin;;
-	*) echo "Unknown target $(uname)";;
+	Linux) set -- x86_64-unknown-linux-musl ;;
+	Darwin) set -- aarch64-apple-darwin x86_64-apple-darwin ;;
+	*) echo "Unknown target $(uname)"; exit 1 ;;
 esac
 
-version=$(git describe --tags)
+for target
+do {
+	version=$(git describe --tags)
 
-curl -LSfs https://japaric.github.io/trust/install.sh |
-    sh -s -- --force --git rust-embedded/cross --tag v0.2.1 --target $target
-command -v cross || PATH=~/.cargo/bin:$PATH
+	cargo=cargo
+	if [ "$target" != aarch64-apple-darwin ]; then
+		curl -LSfs https://japaric.github.io/trust/install.sh |
+		    sh -s -- --force --git rust-embedded/cross --tag v0.2.1 --target $target
+		command -v cross || PATH=~/.cargo/bin:$PATH
+		rustup target add $target
+		cargo=cross
+	fi
+	$cargo build --target $target --release
+	$cargo test  --target $target --release
 
-rustup target add $target
-cross build --target $target --release
-cross test  --target $target --release
+	src=$PWD
+	stage=$(mktemp -d)
 
-src=$PWD
-stage=$(mktemp -d)
+	cp target/$target/release/kak-lsp $stage
+	cp README.asciidoc $stage
+	cp COPYING $stage
+	cp MIT $stage
+	cp UNLICENSE $stage
 
-cp target/$target/release/kak-lsp $stage
-cp README.asciidoc $stage
-cp COPYING $stage
-cp MIT $stage
-cp UNLICENSE $stage
+	cd $stage
+	tar czf $src/kakoune-lsp-$version-$target.tar.gz *
+	cd $src
 
-cd $stage
-tar czf $src/kakoune-lsp-$version-$target.tar.gz *
-cd $src
-
-rm -rf $stage
+	rm -rf $stage
+} done
